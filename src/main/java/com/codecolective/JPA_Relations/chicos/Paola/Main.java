5ff219564fc4aab0a716d5c6214269878a0e671b
Ana <cristinasilvasolis09@gmail.com>
2025-10-03 22:16:42 -0500

    Tarea de pao (prueba)

--- src/main/java/com/codecolective/JPA_Relations/chicos/Paola/Main.java
+++ src/main/java/com/codecolective/JPA_Relations/chicos/Paola/Main.java
@@ -3,5 +3,6 @@ package com.codecolective.JPA_Relations.chicos.Paola;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, Pao");
+        System.out.println("Hola desde el repo clonado");
     }
 }
